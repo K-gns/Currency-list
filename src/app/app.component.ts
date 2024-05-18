df5850b3d33116a68dcfdb41f3726bfb78e60a4c
Filatov Dmitry <Filtv.Dmitry@gmail.com>
2024-05-18 18:04:14 +0500

    V1.0 (no animations)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { CurrencyListComponent } from './currency-list/currency-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [HttpClientModule, CurrencyListComponent],
+  template: '<app-currency-list></app-currency-list>',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'currency-exchange';
-}
+export class AppComponent {}
